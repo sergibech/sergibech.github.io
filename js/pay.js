de75859a41f498a1469c150d45bc5fe1afa328e1
David Rial Figols <davidrialfigols@gmail.com>
2020-04-14 12:12:26 +0200

    primers intents

---
+++ js/pay.js
@@ -0,0 +1,21 @@
+/*Entrada dades de comanda*/
+
+/*Fi entrada dades de comanda*/
+
+
+
+/*Entrada dades d'entrega i pagament*/
+
+/*Fi entrada dades d'entrega i pagament*/
+
+
+
+/*Botó pagar*/
+
+function pagar() {
+  /*
+  if (not (direccio and codi postal and telefon and email)): none
+  if len(telef) != 9: none
+  */
+} 
+/*Fi botó pagar*/
