f2708c70459cd3a0392d2f905dc054bd7fb39170
nballusr <nballusr@gmail.com>
2020-05-02 20:20:55 +0200

    Estil pagina restaurants

--- js/main.js
+++ js/main.js
@@ -79,3 +79,29 @@ $('.carousel .carousel-item').each(function(){
       }
 });
 /*Fi Carousel*/
+
+
+/* Pagina Restaurant */
+$('.radiogroup').change(function(e) {
+	const $this = $(this);
+	var ingredients = document.getElementById("desc-ingredients");
+	ingredients.textContent = $this.attr("ingredients");
+	var añadir = document.getElementById("añadir");
+	añadir.textContent =$this.attr("titol");
+});
+$('.radiogroup2').change(function(e) {
+	const $this = $(this);
+	var ingredients = document.getElementById("desc-ingredients2");
+	ingredients.textContent = $this.attr("ingredients");
+	var añadir = document.getElementById("añadir2");
+	añadir.textContent =$this.attr("titol");
+});
+$('.radiogroup3').change(function(e) {
+	const $this = $(this);
+	var ingredients = document.getElementById("desc-ingredients3");
+	ingredients.textContent = $this.attr("ingredients");
+	var añadir = document.getElementById("añadir3");
+	añadir.textContent =$this.attr("titol");
+});
+/* Fi Pagina Restaurant */
+
